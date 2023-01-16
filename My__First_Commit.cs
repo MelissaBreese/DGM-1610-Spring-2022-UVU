d70525d6ec3cfd3817367538bd02027b8b408fa7
Melissa Breese <melissab495@hotmail.com>
2023-01-15 23:40:44 -0700

    This is my first commit this semester.

---
+++ My__First_Commit.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("This is my first commit. Had to refresh my memory on more details on Console function on w3schools. Hope you don't mind Professor.");    
+    }
+  }
+}
