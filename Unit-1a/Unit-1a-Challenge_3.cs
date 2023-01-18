b3445c531f7140e94dc2e955fde0c357bf69a1fd
Melissa Breese <melissab495@hotmail.com>
2023-01-18 16:55:44 -0700

    Added Unit 1a folder which has all of 1a in it.

---
+++ Unit-1a/Unit-1a-Challenge_3.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 15;
+      Console.WriteLine(!(x > 3 && x < 10)); // returns False because ! (not) is used to reverse the result. Or in otherwords, it's the rules of opposite day when ! is used in this type of Oporator.
+     }
+  }
+}
