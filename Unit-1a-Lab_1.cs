405ec8dd2e22f8bff8b3b4725b67d409a105019e
Melissa Breese <melissab495@hotmail.com>
2023-01-18 01:05:46 -0700

    My Practice with Lab 1a

---
+++ Unit-1a-Lab_1.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "My";
+      Console.WriteLine(name + " name is Melissa");  
+      int age = 28;
+      Console.WriteLine(age);
+      string myText = "Deku IS THE #1 HERO.";
+      Console.WriteLine(myText);
+      string title = "Deku";
+      Console.WriteLine(title + " is my favorite anime character!");
+      string txt = "Deku";
+      Console.WriteLine(txt + " has MANY super powers and is a SUPER HERO!");      
+     }
+  }
+}
