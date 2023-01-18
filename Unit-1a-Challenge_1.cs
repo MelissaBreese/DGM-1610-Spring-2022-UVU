930e52f5b37fff487c6488e81661ba70fefa1f39
Melissa Breese <melissab495@hotmail.com>
2023-01-18 16:38:00 -0700

    I played around with the symbols this time.

---
+++ Unit-1a-Challenge_1.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 3;
+      Console.WriteLine(x < y); // returns True because 5 is greater than 3 I am expieramenting with this
+     }
+  }
+}
+
+
+
+
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 3;
+      Console.WriteLine(x > y); // returns True because 5 is greater than 3
+     }
+  }
+}
+
+
+
+
---
+++ Unit-1a-Challenge_2.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 3;
+      Console.WriteLine(x >= y); // returns False because 5 is neither less than or equal to 3. With this one, I learned it doesn't matter the number, it matters what side of the = you put the </> sign (whichever you're using).
+     }
+  }
+}
