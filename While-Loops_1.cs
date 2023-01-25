1db1aa532d629fefe45c2735de6c693a8c2a7606
Melissa Breese <melissab495@hotmail.com>
2023-01-25 16:51:48 -0700

    Do While Loops 1

---
+++ While-Loops_1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int i = 25;
+      while (i < 150) 
+      {
+        Console.WriteLine(i);
+        i++;
+      }
+    }
+  }
+}
