ffc2e233f11079ffc3f58fb963495481479e53cf
Melissa Breese <melissab495@hotmail.com>
2023-01-25 21:09:04 -0700

    Added For Loops With Arrays Lab 3

---
+++ Unit-1c/For-Loops-With-Arrays-Lab_3.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Some of my favorite Animes: ");
+      string[] animes = {"Fairy Tail", "My Hero Academeia", "Beast Tamer", "One Piece", "Rising Of The Shield Hero"};
+      for (int i = 0; i < animes.Length; i++)
+      {
+      Console.WriteLine(animes[i]);
+      Console.WriteLine(animes.Length);
+      }
+    }
+  }
+}
