c94f9f79ffb3219f6027d98b012a0a69f0d921eb
Melissa Breese <melissab495@hotmail.com>
2023-01-25 17:08:26 -0700

    Added While Loops 2

---
+++ Unit-1c/While-Loops_2.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int i = 1;
+      while (i < 101) 
+      {
+        Console.WriteLine(i);
+        i++;
+      }
+    }
+  }
+}
