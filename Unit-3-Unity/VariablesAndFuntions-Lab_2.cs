77636e24426e924f8f2f778d5990fe2c42f284fb
Melissa Breese <melissab495@hotmail.com>
2023-02-08 18:59:59 -0700

    Added 2nd Variables And Functions Lab

---
+++ Unit-3-Unity/VariablesAndFuntions-Lab_2.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions_4 : MonoBehaviour
+{
+    int myInt = 7;
+
+
+    void Start()
+    {
+        Debug.Log(myInt * 2);
+    }
+
+}
