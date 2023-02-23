1a36675f19b2d7eda522cec8713d004bc9dde092
Melissa Breese <melissab495@hotmail.com>
2023-02-22 19:46:19 -0700

    Added what I understand for Vector3Data -1

---
+++ Unit-4-Unity/Vector3Data-Lab_1.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Vector3Data : ScriptableObject
+{
+    public Vector3 value;
+    public void UpdateValue(Vector3 num)
+    {
+        value += num;
+    }
+}
