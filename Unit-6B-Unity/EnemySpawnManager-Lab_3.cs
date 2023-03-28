3536f062fea65b95c215a260bef101901094baf8
Melissa Breese <melissab495@hotmail.com>
2023-03-27 21:49:56 -0600

    Enemy Spawn Manager Lab 3

---
+++ Unit-6B-Unity/EnemySpawnManager-Lab_3.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    public GameObject[] ufoPrefabs; // Array to store UFO Ships.
+    private float spawnRangeX = 20f;
+    private float spawnPosZ = 20f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0,spawnPosZ);
+            int ufoIndex = Random.Range(0,ufoPrefabs.Length);  // Picks a random UFO from the array.
+            Instantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation);  // Spawns an Indexed UFO from the array at a random location on the X Axi. 
+        }
+    }
+}
