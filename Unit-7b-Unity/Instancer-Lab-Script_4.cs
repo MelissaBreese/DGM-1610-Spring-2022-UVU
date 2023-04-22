537426f8f9a3c6d3707d86fcea5be1711614ad78
Melissa Breese <melissab495@hotmail.com>
2023-04-21 23:56:29 -0600

    Added Instancer Lab Script 4

---
+++ Unit-7b-Unity/Instancer-Lab-Script_4.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Instancer : ScriptableObject
+{
+    public GameObject prefab;
+
+    public void CreateInstance ()
+    {
+        Instantiate(prefab);
+    }
+
+    public void CreatInstance (Vector3Data obj)
+    {
+        Instantiate(prefab, obj.value, Quaternion.identity);
+    }
+
+    public void CreateInstance (Vector3DataList obj)
+    {
+        for (var i = 0; i < obj.vector3DList.Count; i++)
+        {
+            Instantiate(prefab, obj.vector3DList[i].value, Quaternion.identity);
+        }
+    }
+}
